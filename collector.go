2b80f3895beda97549faef436567f92d939c50d1
yc chien <18157220+shiresn@users.noreply.github.com>
2023-11-15 15:55:07 +0800

    change ipmitool timeout args format from base 36 to base 10
    according to ipmitool man page, Options -N only accepts base 10 numbers as input.

--- collector.go
+++ collector.go
@@ -266,7 +266,7 @@ func ipmitoolConfig(config IPMIConfig) []string {
 		args = append(args, "-P", config.Password)
 	}
 	if config.Timeout != 0 {
-		args = append(args, "-N", strconv.FormatInt(config.Timeout, 36))
+		args = append(args, "-N", strconv.FormatInt(config.Timeout, 10))
 	}
 	return args
 }
